5bd9ce4633f19786d82e73ac9f6594f6eb6db5a6
hackerxu <468137306@qq.com>
2015-03-22 11:05:21 +0800

    fix check pro

--- douban/douban_token.py
+++ douban/douban_token.py
@@ -61,8 +61,8 @@ class Doubanfm(object):
         self.get_song()
         if int(self.playingsong['kbps']) != 64:
             self.pro = 1
-        self.playingsong = {}  # 清空列表
-        self.playlist = []
+        # self.playingsong = {}  # 清空列表
+        # self.playlist = []
 
     def win_login(self):
         '''登陆界面'''
