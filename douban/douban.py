49629faf4079b0c2fa0d09c2411e357ff072daa5
hackerxu <468137306@qq.com>
2015-03-26 21:49:42 +0800

    修正tab会被指向的问题

--- douban/douban.py
+++ douban/douban.py
@@ -583,16 +583,20 @@ class History(cli.Cli):
         self.love = colored(' ♥', 'red')
         self.screen_height, self.screen_width = self.linesnum()
 
-        # 3个列表, history rate playlist
+        # 3个tab, history rate playlist
         # 分别对应状态 0 1 2
         self.state = 0
         self.subtitle = ['history_          rate', 'history           rate_']
+        self.play_tag = '♬♬♬♬♬♬'
         # hitory 使用win.history
         self.rate = []
         self.playlist = []
 
         self.get_lines()
         super(History, self).__init__(self.lines)
+        # 默认箭头指向第二排
+        # 为tab留出空余
+        self.markline = 1
         self.win.thread(self.display_help)
         self.run()
 
@@ -604,9 +608,11 @@ class History(cli.Cli):
             for index, i in enumerate(self.win.history):
                 line = i['title'] if len(i['title']) < width else i['title'][:width]
                 line = colored(line, 'green')
-                line = str(index) + ' ' + line
+                line = i['time'][5:] + ' ' + line
                 if i['like'] == 1:
                     line += self.love
+                if i == self.win.playingsong:
+                    line += self.play_tag
                 self.lines.append(line)
         elif self.state == 1:
             self.rate = []
@@ -622,6 +628,8 @@ class History(cli.Cli):
                 line = colored(line, 'green')
                 line = str(index) + ' ' + line
                 line += self.love
+                if i == self.win.playingsong:
+                    line += self.play_tag
 
                 self.lines.append(line)
         elif self.state == 2:
@@ -644,7 +652,7 @@ class History(cli.Cli):
         while True:
             self.display()
             c = getch.getch()
-            if c == self.KEYS['UP']:
+            if c == self.KEYS['UP'] and self.markline != 1:
                 self.updown(-1)
             elif c == self.KEYS['DOWN']:
                 self.updown(1)
@@ -654,7 +662,7 @@ class History(cli.Cli):
             elif c == ' ':
                 self.playsong()
             elif c == self.KEYS['TOP']:      # g键返回顶部
-                self.markline = 0
+                self.markline = 1
                 self.topline = 0
             elif c == self.KEYS['BOTTOM']:   # G键返回底部
                 if len(self.lines) < self.screen_height:
@@ -673,7 +681,7 @@ class History(cli.Cli):
         if self.state == 0:
             # 如果在历史列表里播放,只在win.playlist里插入一首歌曲
             # 播放完毕继续
-            self.win.playlist.insert(0, self.win.history[self.displayline-1])
+            self.win.playlist.insert(0, self.win.history[self.markline + self.topline -1])
         elif self.state == 1:
             self.win.playlist = self.rate[self.displayline-1:]
         elif self.state == 2:
